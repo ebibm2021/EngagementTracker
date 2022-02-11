37ded0a47c0174ace34ab87ffcd7f50d18728592
Sujoy Sett <sujosett@in.ibm.com>
2022-02-11 17:12:24 +0530

    backend core changes

--- backend-core/app.js
+++ backend-core/app.js
@@ -1,20 +1,4 @@
-let instana = require('@instana/collector')({
-  serviceName: process.env.INSTANA_SERVICE_NAME,
-  agentHost: process.env.INSTANA_AGENT_HOST,
-  reportUncaughtException: true
-});
-
 require('dotenv').config();
-let bunyan = require('bunyan');
-// Create your logger(s).
-let bunyanLogger = bunyan.createLogger({name: process.env.INSTANA_SERVICE_NAME});
-// Set the logger Instana should use.
-instana.setLogger(bunyanLogger);
-
-require('lightrun').start({
-  lightrunSecret: '743fd029-7c64-4e38-9902-fe037c7cd788',
-  company: 'icp4aeliteteam',
-});
 
 const express = require('express');
 const app = express();
@@ -28,9 +12,6 @@ app.use(cors());
 
 // Point static path to dist
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, '/dist')));
-app.use('/web/*', express.static(path.join(__dirname, '/dist')));
-app.use('/web', express.static(path.join(__dirname, '/dist')));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set our api routes
--- backend-core/controller/route.controller.js
+++ backend-core/controller/route.controller.js
@@ -1,7 +1,5 @@
 module.exports = function (app, bunyanLogger) {
   const pgdbHandler = require('./pg.controller')
-  const pHandler = require('./prometheus.controller')
-
  
   // PostgreSQL Handlers
   app.get('/api/activity', pgdbHandler.getActivity);
@@ -18,31 +16,4 @@ module.exports = function (app, bunyanLogger) {
   app.post('/api/bulk-upload', pgdbHandler.bulkUpload);
   app.get('/api/filter_groups', pgdbHandler.getFilterGroups)
   app.post('/api/search_analytics', pgdbHandler.searchAnalytics)
-
-  app.get('/metrics', pHandler.getMetrics)
-
-
- 
- 
- 
-  // DB2 Handlers
-  // app.get('/api/activity', db2dbHandler.getActivity);
-  // app.post('/api/activity', db2dbHandler.createActivity);
-  // app.put('/api/activity', db2dbHandler.updateActivity);
-  // app.delete('/api/activity', db2dbHandler.deleteActivity);
-  // app.delete('/api/activities', db2dbHandler.deleteActivities);
-
-  // app.get('/api/engagement', db2dbHandler.getEngagement);
-  // app.post('/api/engagement', db2dbHandler.createEngagement);
-  // app.put('/api/engagement', db2dbHandler.updateEngagement);
-  // app.delete('/api/engagement', db2dbHandler.deleteEngagement);
-
-  // app.post('/api/bulk-upload', db2dbHandler.bulkUpload);
-
-  // app.get('/api/filter_groups', db2dbHandler.getFilterGroups)
-  // app.post('/api/search_analytics', db2dbHandler.searchAnalytics)
-
-  //// STATIC RESOURCES
-  // app.use(express.static(path.join(__dirname, './../public/')));
-  // app.use(express.static(path.join(__dirname, './../dist/')));
 }
